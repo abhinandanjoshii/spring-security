ca686e452ac42002d109eb10b98dd5600aeb8fc0
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2025-09-10 17:34:45 +0530

    in-memory uDetail -> ignore application.properties

--- src/main/java/com/consoleadmin/spring_security/config/SecurityConfig.java
+++ src/main/java/com/consoleadmin/spring_security/config/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,12 +24,33 @@ public class SecurityConfig {
         //enable usernmae pass
         http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
         //form login
-//        http.formLogin(Customizer.withDefaults()); -> remove in case STATELESS
+//        http.formLogin(Customizer.withDefaults()); // -> remove in case STATELESS
         http.httpBasic(Customizer.withDefaults());
         // configure -> no same session continuation
-        http.sessionManagement(Customizer.withDefaults());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user= User
+                .withDefaultPasswordEncoder()
+                .username("consoleadmin")
+                .password("123")
+                .roles("USER")
+                .build();
+
+        UserDetails  admin=User
+                .withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin@789")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user,admin);
     }
 
 }

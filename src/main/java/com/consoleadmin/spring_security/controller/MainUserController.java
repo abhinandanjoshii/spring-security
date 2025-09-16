7b17bca9355bf19a299d7becf7d411e4744feebd
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2025-09-16 09:37:24 +0530

    integrating Bcrypt Encoding

--- src/main/java/com/consoleadmin/spring_security/controller/MainUserController.java
+++ src/main/java/com/consoleadmin/spring_security/controller/MainUserController.java
@@ -3,6 +3,7 @@ package com.consoleadmin.spring_security.controller;
 import com.consoleadmin.spring_security.model.MainUser;
 import com.consoleadmin.spring_security.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,11 @@ public class MainUserController {
 
     @Autowired
     private UserService userService;
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
     @PostMapping("register")
     public MainUser register(@RequestBody MainUser user) {
+        user.setPassword(encoder.encode(user.getPassword()));
         return userService.saveUser(user);
     }
 
